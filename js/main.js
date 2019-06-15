dfd01c8d486aa49093179db17311c65622adf568
albyusSeverus <46374857+albyusSeverus@users.noreply.github.com>
2019-06-15 18:50:09 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function()
+{
+    "use strict";
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+        console.log(top)
+        if(top>=60)
+        {
+            $("header").addClass('second');
+        }
+        else
+        if ($("header").hasClass('second'))
+        {
+           $("header").removeClass('second');
+        }
+        if(top>=910)
+        {
+            $(".paralaxtext").addClass('bounceInLeft');
+
+        }
+        else
+        if ($(".paralaxtext").hasClass('bounceInLeft'))
+        {
+            $(".paralaxtext").removeClass('bounceInLeft')
+        }
+
+
+    });
+
+
+});
